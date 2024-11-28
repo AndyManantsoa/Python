ad95feb2d8f672ef51d131e50a4b33e255a17f2b
andymanantsoa <manantsoaadrs@gmail.com>
2024-11-28 10:04:53 +0530

    updates

---
+++ PRACTICALS/SET1/s1.py
@@ -0,0 +1,5 @@
+#1. A program that converts temperatures from Fahrenheit to Celsius and vice versa.
+
+F= int(input("Enter the temperature in F: "))
+C= (F-32)*5//9
+print("The temperature in D is:",C)
