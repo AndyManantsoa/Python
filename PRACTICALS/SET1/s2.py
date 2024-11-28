4ccaf458a21ce4faa3022f6a22d531bb6db1ff7c
andymanantsoa <manantsoaadrs@gmail.com>
2024-11-28 10:33:15 +0530

    update s2

--- PRACTICALS/SET1/s2.py
+++ PRACTICALS/SET1/s2.py
@@ -1,1 +1,9 @@
 #2. A program that calculates the area and perimeter of a rectangle.
+
+L = int(input("Enter the length of the rectangle: "))
+W = int(input("Enter the width of the rectangle: "))
+
+P = (L+W)*2
+print("The perimeter is ",P)
+A = L*W
+print("The area is ",A)
