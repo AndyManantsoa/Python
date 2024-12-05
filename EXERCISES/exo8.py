4887697b535bc40f604d2664e551cfeda8d14454
andymanantsoa <manantsoaadrs@gmail.com>
2024-12-05 08:39:52 +0530

    updates

--- EXERCISES/exo8.py
+++ EXERCISES/exo8.py
@@ -1,8 +1,16 @@
 #WAP to slice a string
 #string slicing: frontward(positive) & backward(negative)
-#varname[start:end-1:step], by default [0:long:1]
+#varname[start:end+1:step], by default [0:long:1]
 
-a="abcde"
+a="python"
+
 print(a[1:4:2])
 print(a[4:1:-1])
-print(a[0:4:0])
+print(a[-1:-4:-1])
+
+print(a[-1:1:-1])
+print(a[-2:2:-2])
+print(a[2:1:1])
+print(a[-1::-1])
+print(a[::-1])
+print(a[2:6:-1])
