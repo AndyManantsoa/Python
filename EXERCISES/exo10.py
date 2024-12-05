0639b72ec5112724189f92e243c4863fa69c21f7
andymanantsoa <manantsoaadrs@gmail.com>
2024-12-05 09:24:00 +0530

    Exo 10 optimisation

--- EXERCISES/exo10.py
+++ EXERCISES/exo10.py
@@ -1,8 +1,7 @@
 #WAP to check if a string is palindrome or not
 a = input("Enter a string: ")
-b = a[::-1]
 
-if a==b :
+if a==a[::-1] :
     print("It is palindrome")
 else:
     print('It is not palindrome')
